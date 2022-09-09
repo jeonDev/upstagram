0516faafeb60556631e62f95dc4572fd09273dc8
jeonDev <jjhwhdgus0727@gmail.com>
2022-09-09 12:42:43 +0900

    Git 초기세팅

---
+++ upstagram/src/main/java/com/api/upstagram/UpstagramApplication.java
@@ -0,0 +1,13 @@
+package com.api.upstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpstagramApplication.class, args);
+	}
+
+}
---
+++ upstagram/src/test/java/com/api/upstagram/UpstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.upstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
