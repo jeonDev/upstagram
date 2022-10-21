899d7e1fbf5f1575f58ae47560e9581b36bda86c
jeonDev <jjhwhdgus0727@gmail.com>
2022-10-21 23:17:39 +0900

    TODO 제거

--- upstagram/src/main/java/com/api/upstagram/common/vo/User.java
+++ upstagram/src/main/java/com/api/upstagram/common/vo/User.java
@@ -35,37 +35,31 @@ public class User implements UserDetails{
 
     @Override
     public String getPassword() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public String getUsername() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean isEnabled() {
-        // TODO Auto-generated method stub
         return false;
     }
     
