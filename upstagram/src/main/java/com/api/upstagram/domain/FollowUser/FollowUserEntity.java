fc4e88e8ad2a46f3478faa9624dabc1950d4b2c6
jeonDev <jjhwhdgus0727@gmail.com>
2022-10-27 22:14:26 +0900

    Entity Fetch Lazy 변경

--- upstagram/src/main/java/com/api/upstagram/domain/FollowUser/FollowUserEntity.java
+++ upstagram/src/main/java/com/api/upstagram/domain/FollowUser/FollowUserEntity.java
@@ -1,6 +1,7 @@
 package com.api.upstagram.domain.FollowUser;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -25,11 +26,11 @@ public class FollowUserEntity extends BaseEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long followNo;
     
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ID")
     private MemberInfoEntity idMember;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "FOLLOW_ID")
     private MemberInfoEntity followMember;
 
