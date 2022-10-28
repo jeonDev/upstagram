5973904c47cb0f0dcfb9b69610c6865409d08bb4
jhjeon <jhjeon@hanpass.com>
2022-10-28 17:28:17 +0900

    Story Reactiong 추가 수정

--- upstagram/src/main/java/com/api/upstagram/vo/Story/StoryReactionRVO.java
+++ upstagram/src/main/java/com/api/upstagram/vo/Story/StoryReactionRVO.java
@@ -1,5 +1,22 @@
 package com.api.upstagram.vo.Story;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class StoryReactionRVO {
-    
-}
+    private Long reactionNo;
+    private Long storyNo;
+    private String id;
+    private String storyLoveYn;
+    private LocalDateTime storyViewDate;
+
+    @Builder
+    public StoryReactionRVO(Long reactionNo, Long storyNo, String id, String storyLoveYn, LocalDateTime storyViewDate) {
+        this.reactionNo = reactionNo;
+        this.storyNo = storyNo;
+        this.id = id;
+        this.storyLoveYn = storyLoveYn;
+        this.storyViewDate = storyViewDate;
