2cdc04adf239573e367212c162cf030c8c6ac89b
jhjeon <jhjeon@hanpass.com>
2022-11-08 15:24:53 +0900

    Refactoring..

--- upstagram/src/main/java/com/api/upstagram/common/security/OAuth/OAuthAttributes.java
+++ upstagram/src/main/java/com/api/upstagram/common/security/OAuth/OAuthAttributes.java
@@ -44,7 +44,7 @@ public class OAuthAttributes {
                 .name(name)
                 .email(email)
                 .picture(picture)
-                .role(Role.GUEST)
+                .role(Role.USER)
                 .build();
     }
 }
--- upstagram/src/main/java/com/api/upstagram/common/vo/Role.java
+++ upstagram/src/main/java/com/api/upstagram/common/vo/Role.java
@@ -6,8 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-    GUEST("ROLE_GUEST", "손님"),
-    USER("ROLE_USER", "일반 사용자");
+    USER("ROLE_USER", "일반 사용자"),
+    ADMIN("ROLE_ADMIN", "관리자"),
+    MANAGE("ROLE_MANAGE", "광고 담당자");
 
     private final String key;
     private final String title;
