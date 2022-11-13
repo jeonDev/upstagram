1cddae6f45a140f37a299e483304d0e6551ba77d
jeonDev <jjhwhdgus0727@gmail.com>
2022-11-13 16:12:39 +0900

    DM 기능 초기세팅

---
+++ upstagram/src/main/java/com/api/upstagram/vo/Dm/DmChatRoomUserRVO.java
@@ -0,0 +1,14 @@
+package com.api.upstagram.vo.Dm;
+
+import lombok.Data;
+
+@Data
+public class DmChatRoomUserRVO {
+    private Long dmChatRoomNo;
+    private String id;
+    private String name;
+    private String nickname;
+    private String sex;
+    private String tel;
+    private String oauthNo;
+}
