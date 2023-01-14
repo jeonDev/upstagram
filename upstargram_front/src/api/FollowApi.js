8b6bb9a0d1b2d11fa5d7af0b5904b1a2d9ed6066
jeonDev <jjhwhdgus0727@gmail.com>
2023-01-15 00:15:20 +0900

    Follow 컴포넌트 변경 및 신청기능 추가

--- upstargram_front/src/api/FollowApi.js
+++ upstargram_front/src/api/FollowApi.js
@@ -3,10 +3,23 @@ import httpRequest from "../config/httpRequest";
 // Follow 조회
 export async function selectFollowList () {
     return await httpRequest.get('user/follow/list')
-        .then( (response) => {
-            return response.data;
-        })
-        .catch( (error) => {
-            return Promise.reject(error);
-        })
+    .then( (response) => {
+        return response.data;
+    })
+    .catch( (error) => {
+        return Promise.reject(error);
+    })
+}
+
+// Follow 요청
+export async function insertFollowUser (followId) {
+    return await httpRequest.post('user/follow/add', {
+        followId: followId
+    })
+    .then( (response) => {
+        return response.data;
+    })
+    .catch( (error) => {
+        return error.response;
+    })
 }
--- upstargram_front/src/component/Follow/FollowCard.js
+++ upstargram_front/src/component/Follow/FollowCard.js
@@ -1,12 +1,22 @@
 import { Card } from "react-bootstrap";
+import { insertFollowUser } from "../../api/FollowApi";
 
 const FollowCard = (props) => {
-    const {followYn, id, name, nickname, onclickEvent} = props;
+    const {followYn, id, name, nickname, profileImage, onclickEvent} = props;
+
+    // TODO: Follow 여부 체크해서 버튼 변경
+
+    // Follow 요청
+    const requestFollow = async (id) => {
+        const result = await insertFollowUser(id);
+
+        alert(result.message);
+    }
     return (
         <Card variant="outlined" >
             <div className="row" onClick={onclickEvent}>
-                <div className="col-sm-2 align-self-center pointer">
-                    <img alt="이미지" width={'30px;'} src={'/images/mypage.jpg'}/>
+                <div className="col-sm-2 align-self-center pointer text-center">
+                    <img alt="이미지" width={'30px;'} src={profileImage != null ? process.env.REACT_APP_SERVER_FILE_URL + profileImage : '/images/mypage.jpg'}/>
                 </div>
                 <div className="col-sm-6 text-left align-self-center pointer">
                     <div>
@@ -16,9 +26,13 @@ const FollowCard = (props) => {
                         {name}
                     </div>
                 </div>
-                <div className="col-sm-4 align-self-center">
-                    <button className="btn btn-dark pointer">팔로우</button>
+                {
+                followYn === 'Y'
+                &&
+                <div className="col-sm-4 align-self-center text-right">
+                    <button className="btn btn-dark pointer" onClick={() => requestFollow(id)}>팔로우</button>
                 </div>
+                }
             </div>
         </Card>
     );
