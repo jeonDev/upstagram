0704b05e094b11b7ee1e749c669251541a84d7ca
jeonDev <jjhwhdgus0727@gmail.com>
2023-01-15 19:13:22 +0900

    검색화면 수정

--- upstargram_front/src/component/Search/SearchInfoList.js
+++ upstargram_front/src/component/Search/SearchInfoList.js
@@ -4,8 +4,16 @@ import FollowCard from "../Follow/FollowCard";
 const SearchInfoList = (props) => {
     const {searchInfo, searchInfoList} = props;
 
+    // 사용자 상세조회(Mypage)
     const userInfoDetail = (id) => {
+        // TODO: navigate
+        console.log(id);
+    }
 
+    // 해시태그 상세조회(Feed)
+    const hashtagDetail = (hashtag) => {
+        // TODO: navigate
+        console.log(hashtag);
     }
 
     return (
@@ -23,15 +31,13 @@ const SearchInfoList = (props) => {
                         </div>
                     :
                     searchInfoList.map((item, idx) => (
-                        <FollowCard
-                            key={idx}
-                            followYn={'Y'}
-                            id={item.id}
-                            name={item.name}
-                            nickname={item.nickname}
-                            profileImage={item.profileImage}
-                            onclickEvent={() => userInfoDetail(item.id)}
-                        />
+                        <div key={idx} className="m-2">
+                            <FollowCard
+                                followYn={'Y'}
+                                memberInfo={item}
+                                onclickEvent={() => userInfoDetail(item.id)}
+                            />
+                        </div>
                     ))
                 )
                 }
@@ -49,7 +55,18 @@ const SearchInfoList = (props) => {
                     :
                     searchInfoList.map((item, idx) => (
                         <div key={idx}>
-                            {item.feedNo}
+                            <Card className="m-2" onClick={() => hashtagDetail(item.hashtag)}>
+                                <div className="row">
+                                    <div className="col-sm-2 align-self-center pointer text-center">
+                                        <img alt="이미지" width={'30px;'} src="/images/hashtag.png"/>
+                                    </div>
+                                    <div className="col-sm-10 text-left align-self-center pointer">
+                                        <h5>#{item.hashtag}</h5>
+                                        <h6>게시글 {item.hashtagCnt} 건</h6>
+                                    </div>
+                                </div>
+                            </Card>
+                            
                         </div>
                     ))
                 )
