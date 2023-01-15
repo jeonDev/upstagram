df97c6628a7e9037505b6484aefd1ccaf9e60e50
jeonDev <jjhwhdgus0727@gmail.com>
2023-01-15 19:12:57 +0900

    사용자검색 내역 수정

--- upstargram_front/src/component/Follow/FollowCard.js
+++ upstargram_front/src/component/Follow/FollowCard.js
@@ -1,10 +1,8 @@
 import { Card } from "react-bootstrap";
-import { insertFollowUser } from "../../api/FollowApi";
+import { deleteFollowUser, insertFollowUser } from "../../api/FollowApi";
 
 const FollowCard = (props) => {
-    const {followYn, id, name, nickname, profileImage, onclickEvent} = props;
-
-    // TODO: Follow 여부 체크해서 버튼 변경
+    const {followYn, memberInfo, onclickEvent} = props;
 
     // Follow 요청
     const requestFollow = async (id) => {
@@ -12,25 +10,38 @@ const FollowCard = (props) => {
 
         alert(result.message);
     }
+
+    const cancelFollow = async (followNo) => {
+        const result = await deleteFollowUser(followNo);
+
+        alert(result.message);
+    }
     return (
         <Card variant="outlined" >
-            <div className="row" onClick={onclickEvent}>
-                <div className="col-sm-2 align-self-center pointer text-center">
-                    <img alt="이미지" width={'30px;'} src={profileImage != null ? process.env.REACT_APP_SERVER_FILE_URL + profileImage : '/images/mypage.jpg'}/>
+            <div className="row">
+                <div className="col-sm-2 align-self-center pointer text-center" onClick={onclickEvent}>
+                    <img alt="이미지" width={'30px;'} src={memberInfo.profileImage != null ? process.env.REACT_APP_SERVER_FILE_URL + memberInfo.profileImage : '/images/mypage.jpg'}/>
                 </div>
-                <div className="col-sm-6 text-left align-self-center pointer">
+                <div className="col-sm-7 text-left align-self-center pointer" onClick={onclickEvent}>
                     <div>
-                        {nickname}
+                        {memberInfo.nickname}
                     </div>
                     <div>
-                        {name}
+                        {memberInfo.name}
                     </div>
                 </div>
                 {
                 followYn === 'Y'
                 &&
-                <div className="col-sm-4 align-self-center text-right">
-                    <button className="btn btn-dark pointer" onClick={() => requestFollow(id)}>팔로우</button>
+                <div className="col-sm-3 align-self-center text-center">
+                    {
+                        memberInfo.followNo === 0
+                        ?
+                        <button className="btn btn-dark pointer" onClick={() => requestFollow(memberInfo.id)}>팔로우</button>
+                        :
+                        <button className="btn btn-secondary pointer" onClick={() => cancelFollow(memberInfo.followNo)}>팔로우</button>
+                    }
+                    
                 </div>
                 }
             </div>
